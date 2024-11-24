3d38b093503e096ada06b5075ac3da0e0ad5967b
Omary49 <Omaryshaibu49@gmail.com>
2024-11-24 12:59:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,255752264609";
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,255752264609";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923444844060,255752264609";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "255752264609";
 
 global.api_smd = "https://api-smd.onrender.com/" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://suhail-md-vtsf.onrender.com";
